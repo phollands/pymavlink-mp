1c61a2786c10fa9d9afebfe29e32f32d4d68d275
Andrew Tridgell <tridge@samba.org>
2011-02-26 15:52:14 +1100

    added log dump example

---
+++ mavlogdump.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+'''
+example program that dumps a Mavlink log file. The log file is
+assumed to be in the format that qgroundcontrol uses, which consists
+of a series of MAVLink packets, each with a 64 bit timestamp
+header. The timestamp is in microseconds since 1970 (unix epoch)
+'''
+
+import sys, mavlink, struct, time
+
+if len(sys.argv) < 2:
+    print("Usage: mavlogdump.py <LOGFILE>")
+    sys.exit(1)
+
+filename = sys.argv[1]
+
+f = open(filename, mode='r')
+
+# create a mavlink instance, which will do IO on file object 'f'
+mav = mavlink.MAVLink(None)
+
+while True:
+    # read the timestamp
+    tbuf = f.read(8)
+    if len(tbuf) != 8:
+        break
+    (t,) = struct.unpack('>Q', tbuf)
+
+    # read the packet
+    while True:
+        c = f.read(1)
+        m = mav.parse_char(c)
+        if m:
+            print("%s: %s" % (time.ctime(t/1.0e6), m))
+            break
+    
