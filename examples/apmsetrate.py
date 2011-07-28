d31b2c53afc5dd0895c9537cbd25ee2a1be0998e
Andrew Tridgell <tridge@samba.org>
2011-07-29 08:38:19 +1000

    fixed apmsetrate
    now gets target system right, and shows messages

--- examples/apmsetrate.py
+++ examples/apmsetrate.py
@@ -70,6 +70,7 @@ def all_printable(buf):
 
 def wait_heartbeat(m):
     '''wait for a heartbeat so we know the target system IDs'''
+    global target_system, target_component
     print("Waiting for APM heartbeat")
     while True:
         s = m.recv()
@@ -91,6 +92,24 @@ def wait_heartbeat(m):
                 target_component = msg.get_srcComponent()
                 print("Heartbeat from APM (system %u component %u)" % (target_system, target_system))
                 return
+
+def show_messages(m):
+    '''show incoming mavlink messages'''
+    while True:
+        s = m.recv()
+        if len(s) == 0:
+            time.sleep(0.5)
+            continue
+        for c in s:
+            msg = m.mav.parse_char(c)
+            if not msg:
+                continue
+            if msg.get_type() == "BAD_DATA":
+                if all_printable(msg.data):
+                    sys.stdout.write(msg.data)
+                    sys.stdout.flush()
+            else:
+                print(msg)                    
                 
 target_system = 0
 target_component = 0
@@ -102,7 +121,7 @@ master = mavserial(opts.device, baud=opts.baudrate)
 wait_heartbeat(master)
 
 print("Sending all stream request for rate %u" % opts.rate)
-master.mav.request_data_stream_send(target_system, target_component,
-                                    mavlink.MAV_DATA_STREAM_ALL, opts.rate, 1)
-master.mav.request_data_stream_send(target_system, target_component,
-                                    mavlink.MAV_DATA_STREAM_ALL, opts.rate, 1)
+for i in range(0, 3):
+    master.mav.request_data_stream_send(target_system, target_component,
+                                        mavlink.MAV_DATA_STREAM_ALL, opts.rate, 1)
+show_messages(master)
