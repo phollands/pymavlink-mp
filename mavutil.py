075ef1666de97428454272d684a39c6ad915c257
Andrew Tridgell <tridge@samba.org>
2011-08-17 12:42:38 +1000

    unify mavlink parsing under mavfile

--- mavutil.py
+++ mavutil.py
@@ -6,7 +6,7 @@ Copyright Andrew Tridgell 2011
 Released under GNU GPL version 3 or later
 '''
 
-import mavlink, socket, math, struct, time
+import mavlink, socket, math, struct, time, os
 from math import *
 from mavextra import *
 
@@ -28,29 +28,51 @@ def evaluate_condition(condition, vars):
     return v
 
 
-class mavfd(object):
+class mavfile(object):
     '''a generic mavlink port'''
-    def __init__(self, fd, address):
+    def __init__(self, fd, address, source_system=255):
         self.fd = fd
         self.address = address
         self.messages = {}
         self.mav = None
+        self.target_system = 0
+        self.target_component = 0
+        self.mav = mavlink.MAVLink(self, srcSystem=source_system)
+        self.mav.robust_parsing = True
 
     def recv(self):
         '''default recv method'''
         raise RuntimeError('no recv() method supplied')
 
+    def write(self, buf):
+        '''default write method'''
+        raise RuntimeError('no write() method supplied')
+
+    def pre_message(self):
+        '''default pre message call'''
+        return
+
+    def post_message(self, msg):
+        '''default post message call'''
+        msg._timestamp = time.time()
+        return
+
     def recv_msg(self):
         '''message receive routine'''
+        self.pre_message()
         while True:
             s = self.recv()
-            if len(s) == 0:
+            if len(s) == 0 and len(self.mav.buf) == 0:
                 return None
-            for c in s:
-                msg = self.mav.parse_char(c)
-                if msg:
-                    self.messages[msg.get_type()] = msg
-                    return msg
+            msg = self.mav.parse_char(s)
+            if msg:
+                type = msg.get_type()
+                self.messages[type] = msg
+                if type == 'HEARTBEAT':
+                    self.target_system = msg.get_srcSystem()
+                    self.target_component = msg.get_srcComponent()
+                self.post_message(msg)
+                return msg
                 
     def recv_match(self, condition=None, type=None, blocking=False):
         '''recv the next MAVLink message that matches the given condition'''
@@ -68,31 +90,31 @@ class mavfd(object):
             return m
         
 
-class mavserial(mavfd):
+class mavserial(mavfile):
     '''a serial mavlink port'''
     def __init__(self, device, baud=115200, autoreconnect=False, source_system=255):
         import serial
         self.baud = baud
         self.device = device
         self.autoreconnect = autoreconnect
-        self.port = serial.Serial(self.device, self.baud, timeout=1,
+        self.port = serial.Serial(self.device, self.baud, timeout=0,
                                   dsrdtr=False, rtscts=False, xonxoff=False)
 
         try:
             fd = self.port.fileno()
         except Exception:
-            fd = None
-        mavfd.__init__(self, fd, device)
-
-        self.mav = mavlink.MAVLink(self, srcSystem=source_system)
-        self.mav.robust_parsing = True
+            raise
+        mavfile.__init__(self, fd, device, source_system=source_system)
         self.logfile = None
         self.logfile_raw = None
 
     def read(self):
+        n = self.mav.bytes_needed()
         if self.fd is None:
-            return self.port.read(self.port.inWaiting())
-        return self.port.read()
+            waiting = self.port.inWaiting()
+            if waiting < n:
+                n = waiting
+        return self.port.read(n)
 
     def recv(self):
         return self.read()
@@ -122,7 +144,7 @@ class mavserial(mavfd):
                 time.sleep(1)
         
 
-class mavudp(mavfd):
+class mavudp(mavfile):
     '''a UDP mavlink socket'''
     def __init__(self, device, input=False, source_system=255):
         a = device.split(':')
@@ -138,8 +160,7 @@ class mavudp(mavfd):
             self.connected = True
         self.port.setblocking(0)
         self.last_address = None
-        mavfd.__init__(self, self.port.fileno(), device)
-        self.mav = mavlink.MAVLink(self, srcSystem=source_system)
+        mavfile.__init__(self, self.port.fileno(), device, source_system=source_system)
 
     def recv(self):
         try:
@@ -159,96 +180,73 @@ class mavudp(mavfd):
         except socket.error:
             pass
 
-def mavlink_connection(device, baud=115200, source_system=255):
-    '''make a serial or UDP mavlink connection'''
-    if device.find(':') != -1:
-        return mavudp(device, source_system=source_system, input=True)
-    return mavserial(device, baud=baud, source_system=source_system)
 
-
-class mavlogfile(object):
+class mavlogfile(mavfile):
     '''a MAVLink logfile reader/writer'''
     def __init__(self, filename, planner_format=None,
                  write=False, append=False,
-                 robust_parsing=True, notimestamps=False):
+                 robust_parsing=True, notimestamps=False, source_system=255):
         self.filename = filename
-        self.write = write
+        self.writeable = write
         self.robust_parsing = robust_parsing
         self.planner_format = planner_format
         self.notimestamps = notimestamps
         self._two64 = math.pow(2.0, 63)
-        self.mav_mode = (None, None)
         if planner_format is None and self.filename.endswith(".tlog"):
             self.planner_format = True
         mode = 'rb'
-        if self.write:
+        if self.writeable:
             if append:
                 mode = 'ab'
             else:
                 mode = 'wb'
         self.f = open(filename, mode)
-        self.mav = mavlink.MAVLink(None)
-        self.mav.robust_parsing = robust_parsing
-        self.messages = {}
+        mavfile.__init__(self, None, filename, source_system=source_system)
 
-    def read(self):
-        '''read a single MAVLink message'''
+    def recv(self):
+        return self.f.read(self.mav.bytes_needed())
+
+    def pre_message(self):
+        '''read timestamp if needed'''
         # read the timestamp
         if self.notimestamps:
-            t = time.time()
-        elif self.planner_format:
+            return
+        if self.planner_format:
             tbuf = self.f.read(21)
             if len(tbuf) != 21 or tbuf[0] != '-' or tbuf[20] != ':':
-                return None
+                raise RuntimeError('bad planner timestamp %s' % tbuf)
             hnsec = self._two64 + float(tbuf[0:20])
             t = hnsec * 1.0e-7         # convert to seconds
             t -= 719163 * 24 * 60 * 60 # convert to 1970 base
         else:
             tbuf = self.f.read(8)
             if len(tbuf) != 8:
-                return None
+                return
             (tusec,) = struct.unpack('>Q', tbuf)
             t = tusec * 1.0e-6
+        self._timestamp = t
 
+    def post_message(self, msg):
+        '''add timestamp to message'''
+        # read the timestamp
+        if self.notimestamps:
+            msg._timestamp = time.time()
+        else:
+            msg._timestamp = self._timestamp
+        if self.planner_format:
+            self.f.read(1) # trailing newline
 
-        # read the packet
-        while True:
-            c = self.f.read(self.mav.bytes_needed())
-            if c == "" and self.mav.buf == "":
-                return None
-            try:
-                m = self.mav.parse_char(c)
-            except mavlink.MAVError, msg:
-                if self.robust_parsing:
-                    continue
-                raise
-            if not m:
-                continue
-            if self.planner_format:
-                self.f.read(1)
-            if m.get_type() == 'SYS_STATUS':
-                self.mav_mode = (m.mode, m.nav_mode)
-            m._timestamp = t
-            # keep the last message of each type
-            self.messages[m.get_type()] = m
-            return m
-
-    def read_match(self, condition):
-        '''read the next MAVLink message that matches the given condition'''
-        while True:
-            m = self.read()
-            if m is None:
-                return None
-            if not evaluate_condition(condition, self.messages):
-                continue
-            return m
-
-def mav_mode(mode):
-    '''convert a mode specifier to a mode pair'''
-    if mode is None:
-        return None
-    a = mode.split(',')
-    return (int(a[0]), int(a[1]))
+def mavlink_connection(device, baud=115200, source_system=255,
+                       planner_format=None, write=False, append=False,
+                       robust_parsing=True, notimestamps=False):
+    '''make a serial or UDP mavlink connection'''
+    if device.find(':') != -1:
+        return mavudp(device, source_system=source_system, input=True)
+    if os.path.isfile(device):
+        return mavlogfile(device, planner_format=planner_format, write=write,
+                          append=append, robust_parsing=robust_parsing, notimestamps=notimestamps,
+                          source_system=source_system)
+    return mavserial(device, baud=baud, source_system=source_system)
 
 class periodic_event(object):
     '''a class for fixed frequency events'''
