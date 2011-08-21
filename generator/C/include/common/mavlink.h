80b1764d0efb0a620b0d8eb89ffa12f46554528e
Andrew Tridgell <tridge@samba.org>
2011-08-21 17:57:22 +1000

    imported original headers

---
+++ generator/C/include/common/mavlink.h
@@ -0,0 +1,11 @@
+/** @file
+ *	@brief MAVLink comm protocol built from common.xml
+ *	@see http://pixhawk.ethz.ch/software/mavlink
+ *	Generated on Sun Aug 21 17:56:03 2011
+ */
+#ifndef MAVLINK_H
+#define MAVLINK_H
+
+#include "common.h"
+
+#endif // MAVLINK_H
