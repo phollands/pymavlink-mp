5c88b773b45261bec61dd06f754f1f8f73dd1b65
Andrew Tridgell <tridge@samba.org>
2011-08-24 12:09:13 +1000

    regen v1.0 python bindings

--- mavlinkv10.py
+++ mavlinkv10.py
@@ -1851,6 +1851,7 @@ class MAVError(Exception):
         '''MAVLink error class'''
         def __init__(self, msg):
             Exception.__init__(self, msg)
+            self.message = msg
 
 class MAVString(str):
         '''NUL terminated string'''
