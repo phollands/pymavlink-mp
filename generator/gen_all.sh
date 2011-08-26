d7f28ae0ee74ec2e430fc585c9244e929cda4a6f
Andrew Tridgell <tridge@samba.org>
2011-08-26 13:34:12 +1000

    generate all python bindings

--- generator/gen_all.sh
+++ generator/gen_all.sh
@@ -2,10 +2,11 @@
 
 for protocol in 0.9 1.0; do
  for xml in message_definitions/v$protocol/*.xml; do
-     ./mavgen.py --lang=C --wire-protocol=$protocol --output=C/include_v$protocol $xml
+     base=$(basename $xml .xml)
+     ./mavgen.py --lang=C --wire-protocol=$protocol --output=C/include_v$protocol $xml || exit 1
+     ./mavgen.py --lang=python --wire-protocol=$protocol --output=python/mavlink_${base}_v$protocol.py $xml || exit 1
  done
 done
 
-# for mavlink.py just generate for APM for now
-./mavgen.py --lang=python --wire-protocol=0.9 --output=../mavlink.py message_definitions/v0.9/ardupilotmega.xml
-./mavgen.py --lang=python --wire-protocol=1.0 --output=../mavlinkv10.py message_definitions/v1.0/ardupilotmega.xml
+cp -f python/mavlink_ardupilotmega_v0.9.py ../mavlink.py
+cp -f python/mavlink_ardupilotmega_v1.0.py ../mavlinkv10.py
