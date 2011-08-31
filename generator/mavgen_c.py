188881aa50b56d80a4eb571ad07bd0eca8af694a
Andrew Tridgell <tridge@samba.org>
2011-08-31 18:16:01 +1000

    fixup the info structure for the windows compiler

--- generator/mavgen_c.py
+++ generator/mavgen_c.py
@@ -464,7 +464,7 @@ def generate_one(basename, xml):
         if name is not None:
             xml.message_info_array += 'MAVLINK_MESSAGE_INFO_%s, ' % name
         else:
-            xml.message_info_array += '{}, '
+            xml.message_info_array += '{NULL}, '
     xml.message_info_array = xml.message_info_array[:-2]
 
     # add some extra field attributes for convenience with arrays
