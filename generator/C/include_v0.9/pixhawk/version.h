720cd74fe31475906a3a527f70f564b1ebc17eb5
Andrew Tridgell <tridge@samba.org>
2011-08-31 18:16:07 +1000

    regenerated

--- generator/C/include_v0.9/pixhawk/version.h
+++ generator/C/include_v0.9/pixhawk/version.h
@@ -5,7 +5,7 @@
 #ifndef MAVLINK_VERSION_H
 #define MAVLINK_VERSION_H
 
-#define MAVLINK_BUILD_DATE "Wed Aug 31 14:59:14 2011"
+#define MAVLINK_BUILD_DATE "Wed Aug 31 18:15:05 2011"
 #define MAVLINK_WIRE_PROTOCOL_VERSION "0.9"
 
 #include "mavlink.h"
