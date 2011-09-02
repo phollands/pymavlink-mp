4b3f36fb3a32f3dfbf53f54984f0b1aef8a08b81
LM <lm@student.ethz.ch>
2011-09-02 13:51:08 +0200

    Re-generated missing files

--- generator/C/include_v1.0/test/version.h
+++ generator/C/include_v1.0/test/version.h
@@ -5,15 +5,7 @@
 #ifndef MAVLINK_VERSION_H
 #define MAVLINK_VERSION_H
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-#define MAVLINK_BUILD_DATE "Mon Aug 29 10:39:42 2011"
-=======
-#define MAVLINK_BUILD_DATE "Wed Aug 31 18:15:08 2011"
->>>>>>> 720cd74fe31475906a3a527f70f564b1ebc17eb5
-=======
-#define MAVLINK_BUILD_DATE "Fri Sep  2 18:14:41 2011"
->>>>>>> 077b901fe072bb5fefdaca3680b3341c329395cb
+#define MAVLINK_BUILD_DATE "Fri Sep  2 13:50:07 2011"
 #define MAVLINK_WIRE_PROTOCOL_VERSION "1.0"
 
 #include "mavlink.h"
